7b9aa5d6a983c3d319eb0dd934f4e1ced4badced
cpineda <cpineda@teravisiongames.com>
2018-09-12 01:04:13 -0500

    changue elements spawn at the scene

---
+++ AndroidGallery/Assets/Game/Scripts/ButtonController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+    Vector2 floatY;
+    float originalX;
+    float originalY;
+
+    public float floatStrength;
+
+    void Start()
+    {
+        floatStrength = UnityEngine.Random.Range(0.1f, 1f) * floatStrength;
+        this.originalY = this.transform.position.y;
+        this.originalX = this.transform.position.x;
+
+    }
+
+    void Update()
+    {
+        /* Old code:
+        floatY = transform.position;
+        floatY.y = originalY + (Mathf.Sin(Time.time) * floatStrength);
+        transform.position = floatY;
+        */
+        transform.position = new Vector2(originalX,
+            originalY + ((float)Math.Sin(Time.time) * floatStrength));
+    }
+}
--- AndroidGallery/Assets/Game/Scripts/GalleryController.cs
+++ AndroidGallery/Assets/Game/Scripts/GalleryController.cs
@@ -1,4 +1,6 @@
 ﻿using DG.Tweening;
+using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,40 +9,36 @@ public class GalleryController : MonoBehaviour
     public GameObject buttonPrefab;
 
     public Sprite[] gallerySprite;
-    public Image Gallery;
 
-    public Transform scrollContainer_1;
-    public Transform scrollContainer_2;
-    public Transform scrollContainer_3;
+    public RectTransform scrollContainer_1;
+    public RectTransform scrollContainer_2;
+    public RectTransform scrollContainer_3;
 
     // Use this for initialization
     private void Start()
     {
+        var spawnPoints_1 = scrollContainer_1.GetComponentsInChildren<RectTransform>().Where(c => c != scrollContainer_1).OrderBy(a => Guid.NewGuid()).ToArray();
+        var spawnPoints_2 = scrollContainer_2.GetComponentsInChildren<RectTransform>().Where(c => c != scrollContainer_2).OrderBy(a => Guid.NewGuid()).ToArray();
+        var spawnPoints_3 = scrollContainer_3.GetComponentsInChildren<RectTransform>().Where(c => c != scrollContainer_3).OrderBy(a => Guid.NewGuid()).ToArray();
+
         int itemsPerContainer = gallerySprite.Length / 3;
         int lastColumItems = gallerySprite.Length % 3;
 
         for (int i = 0; i < itemsPerContainer; i++)
         {
-            Image newButton = Instantiate(buttonPrefab, scrollContainer_1).GetComponentsInChildren<Image>()[1];
+            Image newButton = Instantiate(buttonPrefab, spawnPoints_1[i].position,Quaternion.identity, scrollContainer_1).GetComponentsInChildren<Image>()[1];
             newButton.sprite = gallerySprite[i];
-            newButton.transform.DOScale(1f, 0.75f).SetLoops(-1, LoopType.Yoyo);
-            newButton.GetComponentInParent<Button>().onClick.AddListener(
-                delegate
-                {
-                    Gallery.sprite = newButton.sprite;
-                    Gallery.transform.parent.gameObject.SetActive(true);
-                });
         }
 
         for (int i = 0; i < itemsPerContainer; i++)
         {
-            Image newButton = Instantiate(buttonPrefab, scrollContainer_3).GetComponentsInChildren<Image>()[1];
+            Image newButton = Instantiate(buttonPrefab, spawnPoints_3[i].position,Quaternion.identity, scrollContainer_3).GetComponentsInChildren<Image>()[1];
             newButton.sprite = gallerySprite[i + itemsPerContainer];
         }
 
         for (int i = 0; i < itemsPerContainer + lastColumItems; i++)
         {
-            Image newButton = Instantiate(buttonPrefab, scrollContainer_2).GetComponentsInChildren<Image>()[1];
+            Image newButton = Instantiate(buttonPrefab, spawnPoints_2[i].position, Quaternion.identity, scrollContainer_2).GetComponentsInChildren<Image>()[1];
             newButton.sprite = gallerySprite[i + (itemsPerContainer * 2)];
         }
     }
@@ -48,5 +46,21 @@ public class GalleryController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+
+    }
+
+
+}
+
+public static class CameraExtensions
+{
+    public static Bounds OrthographicBounds(this Camera camera)
+    {
+        float screenAspect = (float)Screen.width / (float)Screen.height;
+        float cameraHeight = camera.orthographicSize * 2;
+        Bounds bounds = new Bounds(
+            camera.transform.position,
+            new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+        return bounds;
     }
 }
